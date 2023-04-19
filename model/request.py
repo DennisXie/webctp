a846b6b26cc74aca7fd16f88c350c4799a037863
dennis xie <dennisxie1992@outlook.com>
2023-04-19 22:07:24 +0800

    remove model module

--- model/request.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from pydantic.dataclasses import dataclass
-
-@dataclass
-class WebCTPRequest(BaseModel):
-
-    MessageType: str
-    Param: dict[str, any] = {}
-    RequestID: int = 0
